88b2df316ef32fc75f7b190e95def98e3dc91890
Farokh Madah <fa.madah@gmail.com>
2024-08-30 15:31:05 +0330

    cross origin

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,27 @@ const nextConfig = {
     config.externals.push('pino-pretty', /* add any other modules that might be causing the error */);
     return config;
   },
+  crossOrigin: 'anonymous', // or 'use-credentials'
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        // headers: [
+        //   {
+        //     key: "Cross-Origin-Opener-Policy",
+        //     value: "same-origin-allow-popups"
+        //   },
+        // ],
+        // source: "/api/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
