4ada75ad08afe8a76f7570be10849337b5d143fa
Farokh Madah <fa.madah@gmail.com>
2024-08-30 21:34:32 +0330

    ui

--- app/(root)/collections/[collectionId]/page.tsx
+++ app/(root)/collections/[collectionId]/page.tsx
@@ -14,13 +14,13 @@ const CollectionDetails = async ({
     <div className="px-10 py-5 flex flex-col items-center gap-8">
       <Image
         src={collectionDetails.image}
-        width={1500}
-        height={1000}
+        width={100}
+        height={100}
         alt="collection"
-        className="w-full h-[400px] object-cover rounded-xl"
+        className="w-full h-[100px] object-contain rounded-xl"
       />
-      <p className="text-heading3-bold text-grey-2">{collectionDetails.title}</p>
-      <p className="text-body-normal text-grey-2 text-center max-w-[900px]">{collectionDetails.description}</p>
+      {/* <p className="text-heading1-bold text-grey-2">{collectionDetails.title}</p> */}
+      <p className="text-heading3-bold text-grey-2 text-center max-w-[900px]">{collectionDetails.description}</p>
       <div className="flex flex-wrap gap-16 justify-center">
         {collectionDetails.gifts.map((gift: GiftType) => (
           <GiftCard key={gift._id} gift={gift} />
