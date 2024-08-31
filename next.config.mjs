438fe94f05914d9f46212088be8c5b0a275cfc39
Farokh Madah <fa.madah@gmail.com>
2024-08-31 23:18:46 +0330

    enable CORS

--- next.config.mjs
+++ next.config.mjs
@@ -17,18 +17,18 @@ const nextConfig = {
     config.externals.push('pino-pretty', /* add any other modules that might be causing the error */);
     return config;
   },
-  // crossOrigin: 'anonymous', // or 'use-credentials'
+  crossOrigin: 'anonymous', // or 'use-credentials'
   async headers() {
     return [
       {
         // matching all API routes
-        source: "/(.*)",
-        // source: "/api/:path*",
+        // source: "/(.*)",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Authorization, Date, X-Api-Version" },
         ]
       }
     ]
