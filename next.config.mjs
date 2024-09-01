0d264e7512481806e4269376b39eb998bd4c2449
Farokh Madah <fa.madah@gmail.com>
2024-09-01 19:38:12 +0330

    remove CORS

--- next.config.mjs
+++ next.config.mjs
@@ -16,22 +16,6 @@ const nextConfig = {
   webpack: (config) => {
     config.externals.push('pino-pretty', /* add any other modules that might be causing the error */);
     return config;
-  },
-  // crossOrigin: 'anonymous', // or 'use-credentials'
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        // source: "/(.*)",
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Authorization, Date, X-Api-Version" },
-        ]
-      }
-    ]
   }
 };
 
